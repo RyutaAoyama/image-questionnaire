aa2a25c55f447d536bf895a5f210c1cc24f9486e
RyutaAoyama <tsuisen11@gmail.com>
2019-11-27 03:48:46 +0900

    メールアドレスで会員登録機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get 'users' => "users#index"
+  get "users/:id"=> "users#show"
+
   resources :enquets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "tops#top"
